73321291e3e03008ac07d96dece65831e9e9bed9
Ben R <robinsonbena@gmail.com>
2023-05-21 10:21:41 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: 'a@b.com', password: '123456')
+User.create(email: 'b@c.com', password: '123456')
